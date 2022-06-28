64c080e516b47c18a73afe7b3fb850b6493d6c04
Rockheung <rockheung@gmail.com>
2022-06-28 21:59:39 +0900

    init page serving

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+app.use(express.static("public"));
 app.use((req, res, next) => {
   if (req.method === "HEAD") {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -8,7 +15,13 @@ app.use((req, res, next) => {
 });
 
 app.use("/", (req, res) => {
-  res.send("good");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(3000);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
