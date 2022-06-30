c9f9f7e691e429b3cf80cf9a92064071ba7bc9f5
Rockheung <rockheung@gmail.com>
2022-06-30 22:56:58 +0900

    update layout

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ import io from "./socket.io.esm.min.js";
 
 async function main() {
   const socket = io();
+  socket.on('connect', () => {
+    
+  })
 }
 
 main().catch(console.error);
--- server.js
+++ server.js
@@ -4,18 +4,27 @@ const http = require("http");
 const path = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
 
-app.use(express.static("public"));
+const io = new Server(server, {
+  cookie: {
+    name: "socket-id",
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 86400
+  }
+});
+
 app.use((req, res, next) => {
   if (req.method === "HEAD") {
     res.setHeader("Access-Control-Allow-Origin", "*");
   }
   next();
 });
+app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("a user connected", socket.id);
+  console.log(socket.rooms)
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
